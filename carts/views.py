69bfdf639b9030be8ee739cabd90149dbc37df10
rohit06-stack <rohitvajpayee2001@gmail.com>
2024-01-25 21:32:29 +0530

    added new files

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from store.models import Product
 from .models import Cart,CartItem
 
@@ -29,14 +29,14 @@ def add_cart(request,product_id):
 
 
 
-def carts(request,total=0,quantity=0,cart_item=None):
+def carts(request,total=0,quantity=0,cart_items=None):
     try:
         cart = Cart.objects.get(card_id=_cart_id(request))
         cart_items =CartItem.objects.filter(cart= cart,is_active=True)
         for cart_item in cart_items:
             total += (cart_item.product.price*cart_item.quantity)
             quantity +=cart_item.quantity
-    except cart.DoesNotExist as e:
+    except Cart.DoesNotExist as e:
         print(e)
     context =  {
         'total':total,
@@ -44,3 +44,30 @@ def carts(request,total=0,quantity=0,cart_item=None):
         'cart_items': cart_items
     }
     return render(request,'store/carts.html',context)
+
+def remove_cart(request,product_id):
+    cart = Cart.objects.get(card_id = _cart_id(request))
+    product = get_object_or_404(Product,id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if(cart_item.quantity >1):
+        cart_item.quantity -=1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('carts')
+
+def remove_cart_item(request,product_id):
+    cart = Cart.objects.get(card_id=_cart_id(request))
+    product = get_object_or_404(Product,id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('carts')
+
+def add_cart_item(request,product_id):
+    cart = Cart.objects.get(card_id = _cart_id(request))
+    product = get_object_or_404(Product,id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if(cart_item.quantity >=1):
+        cart_item.quantity +=1
+        cart_item.save()
+    return redirect('carts')
--- store/views.py
+++ store/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
 from .models import Product
 from category.models import Category
-
+from carts.models import CartItem
+from carts.views import _cart_id
+from django.core.paginator import Paginator
 # Create your views here.
 def store(request,category_slug = None):
     categories = None
@@ -9,12 +12,18 @@ def store(request,category_slug = None):
     if(category_slug != None):
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories,is_available=True)
+        paginator = Paginator(products,3)
+        page = request.GET.get('page')
+        page_products = paginator.get_page(page)
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products,3)
+        page = request.GET.get('page')
+        page_products = paginator.get_page(page)
         product_count = products.count()
     context = {
-          'products': products,
+          'products': page_products,
           'product_count': product_count
         }
     return render(request,'store/store.html',context)
@@ -22,10 +31,14 @@ def store(request,category_slug = None):
 def product_detail(request,category_slug,product_slug):
     try:
         single_product=Product.objects.get(category__slug=category_slug,slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__card_id= _cart_id(request), product = single_product).exists()
+        # return HttpResponse(in_cart)
+        # exit()
     except Exception as e:
         raise e 
 
     context={
-        'single_product': single_product
+        'single_product': single_product,
+        'in_cart': in_cart
     }
     return render(request,'store/product_detail.html',context)
