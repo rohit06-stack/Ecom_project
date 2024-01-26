9602d4e01f3ab1f51497f06f14c9b2b973a2fb98
rohit06-stack <rohitvajpayee2001@gmail.com>
2024-01-27 01:01:40 +0530

    new updates

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
+class Account(AbstractBaseUser):
+    first_name = models.CharField(max_length=50),
+    last_name = models.CharField(max_length=50),
+    username = models.CharField(max_length=50, unique=True),
+    phone_number = models.CharField(max_length=50),
+
+    # required field
+    date_joined = models.DateTimeField(auto_now_add=True),
+    last_login =  models.DateTimeField(auto_now_add=True),
+    is_admin = models.BooleanField(default=False),
+    is_staff = models.BooleanField(default=False),
+    is_active = models.BooleanField(default=False),
+    is_superadmin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username','first_name','last_name']
+
+    def __str__(self):
+        return self.email 
--- myEcomStore/settings.py
+++ myEcomStore/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'category',
     'store',
-    'carts'
+    'carts',
+    'accounts'
 ]
 
 MIDDLEWARE = [
