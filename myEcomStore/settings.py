30bd9d8a450cac342b985f83d932f2aacf2308a2
rohit06-stack <rohitvajpayee2001@gmail.com>
2024-02-10 00:50:16 +0530

    new updates

--- myEcomStore/settings.py
+++ myEcomStore/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'category',
     'store',
     'carts',
-    'accounts'
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -77,6 +77,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'myEcomStore.wsgi.application'
+AUTH_USER_MODEL = 'accounts.Account'
 
 
 # Database
@@ -133,3 +134,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR : "danger",
+}
